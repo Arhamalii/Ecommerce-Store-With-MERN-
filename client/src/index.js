dcc02345098d559ebdf2c0d30e131c9c349f39fc
Arhamalii <arhampersonal786@gmail.com>
2023-08-28 21:27:09 +0500

    ready to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { CartPovider } from "./context/Cart";
 import { AuthProvider } from "./context/auth";
 import { SearchPovider } from "./context/search";
-import { CartPovider } from "./context/Cart";
 
 import "./index.css";
 
